a8c19e5b5606898a8d6c1213a6fc5a01db304c14
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-14 18:50:57 +0100

    Add openzeppelin imports

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
 contract MyEpicGame {
