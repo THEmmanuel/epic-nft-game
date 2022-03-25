a0bb3d25367763393d64ac3f308da6a38ba621b5
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-25 16:21:50 +0100

    update smart contract and added comments

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,190 +1,292 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-// String encoding libraries
-import { Base64 } from "./libraries/Base64.sol";
+// Helper we wrote to encode in Base64
+import "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
-
+// Our contract inherits from ERC721, which is the standard NFT contract!
 contract MyEpicGame is ERC721 {
-
-  struct CharacterAttributes {
-    string name;
-    string imageURI;        
-    uint hp;
-    uint maxHp;
-    uint attackDamage;
-    uint CharacterIndex;
-
-  }
-
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-  mapping(uint => CharacterAttributes) public defaultAttributes;
-  CharacterAttributes[] defaultCharacters;
-  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
-
-  struct BigBoss {
-    string name;
-    string imageURI;
-    uint hp;
-    uint maxHp;
-    uint attackDamage;
-  }
-  BigBoss public bigBoss;
-
-  mapping(address => uint256) public nftHolders;
-
-  constructor(
-    string[] memory characterNames,
-    string[] memory characterImageURIs,
-    uint[] memory characterHp,
-    uint[] memory characterAttackDmg,
-    string memory bossName,
-    string memory bossImageURI,
-    uint bossHp,
-    uint bossAttackDmg
-  )
-    ERC721("Heroes", "HERO")
-  {
-    bigBoss = BigBoss({
-      name: bossName,
-      imageURI: bossImageURI,
-      hp: bossHp,
-      maxHp: bossHp,
-      attackDamage: bossAttackDmg
-    });
-
-    console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);
-
-    for(uint i = 0; i < characterIds.length; i += 1) {
-      defaultAttributes[characterIds[i]] = CharacterAttributes({
-        name: characterNames[i],
-        imageURI: characterImageURIs[i],
-        hp: characterHp[i],
-        attackDamage: characterAttackDmg[i]
-      });
-
-      CharacterAttributes memory c = defaultAttributes[characterIds[i]];
-      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
-    }
-    _tokenIds.increment();
-  };
-
-  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
-
-    string memory strHp = Strings.toString(charAttributes.hp);
-    string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
-
-    string memory json = Base64.encode(
-      bytes(
-        string(
-          abi.encodePacked(
-            '{"name": "',
-            charAttributes.name,
-            ' -- NFT #: ',
-            Strings.toString(_tokenId),
-            '", "description": "CriticalHit is a turn-based NFT game where you take turns to attack the boos.", "image": "',
-            charAttributes.imageURI,
-            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value": 300 }, { "trait_type": "Attack Damage", "value": ',
-            strAttackDamage,', "max_value": 400} ]}'
-          )
-        )
-      )
-    );
-
-    string memory output = string(
-      abi.encodePacked("data:application/json;base64,", json)
-    );
-    
-    return output;
-  };
-
-  function mintCharacterNFT(uint _characterIndex) external {
-    uint256 newItemId = _tokenIds.current();
-    _safeMint(msg.sender, newItemId);
-    nftHolderAttributes[newItemId] = CharacterAttributes({
-      characterIndex: _characterIndex,
-      name: defaultCharacters[_characterIndex].name,
-      imageURI: defaultCharacters[_characterIndex].imageURI,
-      hp: defaultCharacters[_characterIndex].hp,
-      maxHp: defaultCharacters[_characterIndex].maxHp,
-      attackDamage: defaultCharacters[_characterIndex].attackDamage
-    });
-
-    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);    
-    nftHolders[msg.sender] = newItemId;
-    _tokenIds.increment();
-    emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
-  }
-
-  function attackBoss() public {
-    // Get the state of the player's NFT.
-    uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
-    CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
-
-    console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
-    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
-    
-    // Make sure the player has more than 0 HP.
-    require (
-        player.hp > 0,
-        "Error: character must have HP to attack boss."
-    );
-
-    // Make sure the boss has more than 0 HP.
-    require (
-        bigBoss.hp > 0,
-        "Error: boss must have HP to attack boss."
-    );
-    
-    // Allow player to attack boss.
-    if (bigBoss.hp < player.attackDamage) {
-        bigBoss.hp = 0;
-    } else {
-        bigBoss.hp = bigBoss.hp - player.attackDamage;
+    // We'll hold our character's attributes in a struct. Feel free to add
+    // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
+    struct CharacterAttributes {
+        // the data in the constructor is stored in this struct
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
     }
 
-    // Allow boss to attack player.
-    if (player.hp < bigBoss.attackDamage) {
-        player.hp = 0;
-    } else {
-        player.hp = player.hp - bigBoss.attackDamage;
+    // a struct to hold the boss data and a BigBoss variable to hold our boss so that we can ref it indiff functions
+    struct BigBoss {
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
     }
-    
-    // Console for ease.
-    console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
-    console.log("Boss attacked player. New player hp: %s\n", player.hp);
-  };
 
-  function checkIfUserHasNFT() public view returns (CharacterAttributes memory) {
-        // Get the tokenId of the user's character NFT
-        uint256 userNftTokenId = nftHolders[msg.sender];
+    BigBoss public bigBoss;
 
-        // If the user has a tokenId in the map, return their character.
-        if (userNftTokenId > 0) {
-            return nftHolderAttributes[userNftTokenId];
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // A lil array to help us hold the default data for our characters.
+    // This will be helpful when we mint new characters and need to know
+    // things like their HP, AD, etc.
+    CharacterAttributes[] defaultCharacters;
+
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
+
+    //EVENTS....
+    // fire this when we finish minting an nft for the user. allows us to notify them that minting is done
+    event CharacterNFTMinted(
+        address sender,
+        uint256 tokenId,
+        uint256 characterIndex
+    );
+    // fire this when the attack is complete
+    event AttackComplete(uint newBossHp, uint newPlayerHp);
+
+    // Data passed in to the contract when it's first created initializing the characters.
+    // We're going to actually pass these values in from from run.js.
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg,
+        string memory bossName, // These new variables would be passed in via run.js or deploy.js.
+        string memory bossImageURI,
+        uint bossHp,
+        uint bossAttackDamage
+    ) ERC721("Heroes", "HERO") {
+        // Initialize the boss. Save it to our global "bigBoss" state variable.
+        bigBoss = BigBoss({
+            name: bossName,
+            imageURI: bossImageURI,
+            hp: bossHp,
+            maxHp: bossHp,
+            attackDamage: bossAttackDamage
+        });
+
+        console.log(
+            "Done initializing boss %s w/ HP %s, img %s",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.imageURI
+        );
+
+        // Loop through all the characters, and save their values in our contract so
+        // we can use them later when we mint our NFTs.
+        for (uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+
+            // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
         }
 
+        // I increment tokenIds here so that my first NFT has an ID of 1.
+        // More on this in the lesson!
+        _tokenIds.increment();
+    }
+
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function mintCharacterNFT(uint _characterIndex) external {
+        // Get current tokenId (starts at 1 since we incremented in the constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // The magical function! Assigns the tokenId to the caller's wallet address.
+        _safeMint(msg.sender, newItemId);
+
+        // We map the tokenId => their character attributes. More on this in
+        // the lesson below.
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // Keep an easy way to see who owns what NFT.
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
+
+        // event above is fired
+        emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ]; // this retreives the specific NFT data by querying for it using the tokenid we passed into the function
+        // then we will take all the data and put it in a variable called json
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            bytes( // so this is the structure of the json file and by doing it like this we're dynamically setting it up so that its easy to chnage things up
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        charAttributes.name,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                        charAttributes.imageURI,
+                        '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                        strHp,
+                        ', "max_value":',
+                        strMaxHp,
+                        '}, { "trait_type": "Attack Damage", "value": ',
+                        strAttackDamage,
+                        "} ]}"
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+            // so basically when we run this, this block encodes and formats our json file into base64 making it readable by our browser
+        );
+
+        return output;
+    }
+
+    function attackBoss() public {
+        // Get the state of the player's NFT.
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender]; //get the holders id
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ]; //get players attributes
+
+        console.log(
+            "\nPlayer w/ character %s about to attack. Has %s HP and %s AD",
+            player.name,
+            player.hp,
+            player.attackDamage
+        );
+        console.log(
+            "Boss %s has %s HP and %s AD",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.attackDamage
+        );
+
+        // Make sure the player has more than 0 HP.
+        require(player.hp > 0, "Error: character must have HP to attack boss.");
+
+        // Make sure the boss has more than 0 HP.
+        require(bigBoss.hp > 0, "Error: boss must have HP to attack boss.");
+
+        // Allow player to attack boss.
+        if (bigBoss.hp < player.attackDamage) {
+            bigBoss.hp = 0; // maually set it to 0 so that it doesnt become negative
+        } else {
+            bigBoss.hp = bigBoss.hp - player.attackDamage; // if not continue to reduce the boss' hp based on how much damage the player attcks her
+        }
+
+        // Allow the boss to attack a player
+        if (player.hp < bigBoss.attackDamage) {
+            player.hp = 0; // player cant go bellow 0 or it will also throw an error
+        } else {
+            player.hp = player.hp - bigBoss.attackDamage;
+        }
+
+        // Console for ease.
+        console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+        console.log("Boss attacked player. New player hp: %s\n", player.hp);
+
+        //attack event fired
+        emit AttackComplete(bigBoss.hp, player.hp);
+    }
+
+    // checks of user has an NFT and that their token id isnt 0
+    function checkIfUserHasNFT()
+        public
+        view
+        returns (CharacterAttributes memory)
+    {
+        // Get the tokenId of the user's character NFT
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        // If the user has a tokenId in the map, return their character.
+        if (userNftTokenId > 0) {
+            // since there is no way to check of a key in a map exists, we set the default value to 0
+            // this can be problematic for a user that why above we have our .increment() so that no use is allowed to have a tokenid of 0
+            return nftHolderAttributes[userNftTokenId];
+        }
         // Else, return an empty character.
         else {
             CharacterAttributes memory emptyStruct;
             return emptyStruct;
-        } 
-  };
+        }
+    }
 
-  function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory) {
+    // for new players to choose a character nft they want to mint
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
         return defaultCharacters;
-  };
+    }
 
-  function getBigBoss() public view returns (BigBoss memory) {
+    // this will retrieve the boss showing their name, hp and image or whatever
+    function getBigBoss() public view returns (BigBoss memory) {
         return bigBoss;
-    };
+    }
 }
